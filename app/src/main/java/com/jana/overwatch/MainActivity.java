ab732daaabb51d67d853be13cf2129eadfe1796b
SeongwooJ <deram9090@hotmail.com>
2016-09-24 20:09:27 +1200

    Login screen added

--- app/src/main/java/com/jana/overwatch/MainActivity.java
+++ app/src/main/java/com/jana/overwatch/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jana.overwatch;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ListView mListView;
+    private FloatingActionButton mAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mListView = (ListView) findViewById(R.id.listView);
+        mAddButton = (FloatingActionButton) findViewById(R.id.addButton);
 
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
         Target room1 = new Target(R.drawable.placeholder, "Room 1");
         Target bathroom = new Target(R.drawable.placeholder, "Bathroom");
 
