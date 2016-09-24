ac92e4c2b797f3b8e3a4e9dbe27336a3f54bbe19
SeongwooJ <deram9090@hotmail.com>
2016-09-24 21:45:37 +1200

    added beans list and notification ui

--- app/src/main/java/com/jana/overwatch/MainActivity.java
+++ app/src/main/java/com/jana/overwatch/MainActivity.java
@@ -19,31 +19,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mListView = (ListView) findViewById(R.id.listView);
-        mAddButton = (FloatingActionButton) findViewById(R.id.addButton);
-
-        mAddButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                
-            }
-        });
-        Target room1 = new Target(R.drawable.placeholder, "Room 1");
-        Target bathroom = new Target(R.drawable.placeholder, "Bathroom");
-
-        ArrayList<Target> targetList = new ArrayList<>();
-        targetList.add(room1);
-        targetList.add(bathroom);
-
-        MainListAdapter adapter = new MainListAdapter(this, targetList);
-        mListView.setAdapter(adapter);
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                int itemPosition = i;
-
-            }
-        });
     }
 }
